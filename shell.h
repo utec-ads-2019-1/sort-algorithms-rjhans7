61df7c97e5ac8c55df8643cdc5a60e0ea4abae7d
Rjhans7 <roosevelt.ubaldo@outlook.com>
2019-04-11 11:26:32 -0500

    Shell working

--- shell.h
+++ shell.h
@@ -7,8 +7,12 @@ class ShellSort : public Sort {
     public:
         ShellSort(void *elements, size_t size) : Sort(elements, size) {}
 
-        void execute(void (*compare)(void*, int, int)) {
-            // TODO
+        void execute(void (*compare)(void*, int, int)){
+
+            for (int gap = size/2; gap > 0; gap /=2 )
+                for (int i=gap; i<size; ++i)
+                    compare(elements,i,gap);
+
         }
 
         inline string name() { return "ShellSort"; }
